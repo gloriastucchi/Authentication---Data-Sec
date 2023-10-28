054b96d894d35ca6bfc3f8a590f9083c6bbd7b05
Leonardo Faggion <76787659+leofagg@users.noreply.github.com>
2023-10-28 12:59:18 +0200

    app server Co-authored-by: Cesare Maio <cesare.maio@gmail.com> Co-authored-by: gloriastucchi <gloriastucchi@users.noreply.github.com>

--- src/server/compute/HelloService.java
+++
@@ -1,8 +0,0 @@
-package compute;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloService extends Remote {
-   public String echo(String input) throws RemoteException;
-}
